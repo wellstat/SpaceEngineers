bab452615e4053e7f33f6888be3f21e3a34faaeb
wellstat <35619900+wellstat@users.noreply.github.com>
2018-04-14 12:29:11 +0800

    Delete RaycastManager.cs

--- IngameScripts/Utilities/RaycastManager.cs
+++
@@ -1,4 +0,0 @@
-﻿//
-// Renamed class to RaycastTracker.cs to better describe the function it does. New link below:
-// https://github.com/wellstat/SpaceEngineers/blob/master/IngameScripts/Utilities/RaycastTracker.cs
-//
